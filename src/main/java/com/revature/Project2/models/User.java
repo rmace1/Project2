3ec0989b07125e0db9c2fe2008feb52ef65bb47d
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2021-12-29 22:18:12 -0500

    model finished

--- src/main/java/com/revature/Project2/models/User.java
+++ src/main/java/com/revature/Project2/models/User.java
@@ -1,9 +1,27 @@
 package com.revature.Project2.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "User")
 public class User {
-    Integer Id;
-    String userName;
-    String firstName;
-    String lastName;
-    String profilePicUrl;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String userName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String profilePic;
+    @Column(nullable = false)
+    private String password;
 }
