d502b2c08848294a6fe493ad1da07f84ea9cca7a
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2022-01-01 12:45:37 -0500

    transactional

--- src/main/java/com/revature/Project2/services/UserService.java
+++ src/main/java/com/revature/Project2/services/UserService.java
@@ -5,9 +5,11 @@ import com.revature.Project2.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserService {
     @Autowired
     private UserRepo userRepo;
